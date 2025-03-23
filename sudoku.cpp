7016698ce485b6373a0bc27c9aaa46ac849edc84
Siddhi Raney <164665691+SiddhiRaney@users.noreply.github.com>
2025-03-23 21:33:18 +0530

    Create sudoku.cpp

---
+++ sudoku.cpp
@@ -0,0 +1,74 @@
+#include <iostream>
+using namespace std;
+
+#define N 9
+
+// Check if placing num at board[row][col] is valid
+bool isValid(int board[N][N], int row, int col, int num) {
+    for (int i = 0; i < N; i++) {
+        // Check row and column
+        if (board[row][i] == num || board[i][col] == num)
+            return false;
+    }
+
+    // Check 3x3 sub-grid
+    int startRow = row - row % 3, startCol = col - col % 3;
+    for (int i = 0; i < 3; i++) {
+        for (int j = 0; j < 3; j++) {
+            if (board[i + startRow][j + startCol] == num)
+                return false;
+        }
+    }
+    return true;
+}
+
+// Recursive backtracking function to solve Sudoku
+bool solveSudoku(int board[N][N]) {
+    for (int row = 0; row < N; row++) {
+        for (int col = 0; col < N; col++) {
+            // Find empty cell
+            if (board[row][col] == 0) {
+                for (int num = 1; num <= 9; num++) {
+                    if (isValid(board, row, col, num)) {
+                        board[row][col] = num;
+                        if (solveSudoku(board)) // Recursion
+                            return true;
+                        board[row][col] = 0; // Backtrack
+                    }
+                }
+                return false; // No valid number found
+            }
+        }
+    }
+    return true; // Solved successfully
+}
+
+// Print Sudoku board
+void printBoard(int board[N][N]) {
+    for (int i = 0; i < N; i++) {
+        for (int j = 0; j < N; j++)
+            cout << board[i][j] << " ";
+        cout << endl;
+    }
+}
+
+int main() {
+    int board[N][N] = {
+        {5, 3, 0, 0, 7, 0, 0, 0, 0},
+        {6, 0, 0, 1, 9, 5, 0, 0, 0},
+        {0, 9, 8, 0, 0, 0, 0, 6, 0},
+        {8, 0, 0, 0, 6, 0, 0, 0, 3},
+        {4, 0, 0, 8, 0, 3, 0, 0, 1},
+        {7, 0, 0, 0, 2, 0, 0, 0, 6},
+        {0, 6, 0, 0, 0, 0, 2, 8, 0},
+        {0, 0, 0, 4, 1, 9, 0, 0, 5},
+        {0, 0, 0, 0, 8, 0, 0, 7, 9}
+    };
+
+    if (solveSudoku(board))
+        printBoard(board);
+    else
+        cout << "No solution exists!" << endl;
+
+    return 0;
+}
